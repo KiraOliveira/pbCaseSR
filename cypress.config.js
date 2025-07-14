085d6542215a4caed3885404be21818eb7c99c63
Kira Oliveira <kira.oliveira.web@gmail.com>
2025-07-14 14:07:18 -0300

    Finalizado desenvolvimento e testes realizados

--- cypress.config.js
+++ cypress.config.js
@@ -4,11 +4,23 @@ const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = defineConfig({
+  fixturesFolder: false, 
   e2e: {
-    specPattern: "cypress/e2e/step_definitions/*.feature",
+    specPattern: "cypress/e2e/step_definitions/*",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      on('file:preprocessor', cucumber())  
-    }
+       //implement node event listeners here
+      on('file:preprocessor', cucumber()) 
+    },
+    env: {
+      hhideXhr: true
+    },
+    "baseUrl": 'https://the-internet.herokuapp.com/challenging_dom'
   },
+  "video": true,
+  "viewportWidth": 1366,
+  "viewportHeight": 768,
+  "defaultCommandTimeout": 10000,
+  "pageLoadTimeout": 60000,
+  "failOnStatusCode": false,
+  "chromeWebSecurity": false
 });
--- cypress/e2e/step_definitions/steps.feature
+++ cypress/e2e/step_definitions/steps.feature
@@ -1,9 +1,6 @@
-Feature: Autenticar usuário
-    Como um usuário que possui conta no sistema
-    Eu quero realizar autenticação
-    Para que eu possa acessar minha conta
+Feature: Site Challenging DOM
 
-Scenario: Login de usuário com sucesso
-    Given Eu acesso a página de login
-    When Eu preencho os dados e solicito acesso
-    Then Sistema faz a autenticação com sucesso
+    Scenario: Acessar o site Challenging DOM
+        Given Acessar o site Challenging DOM
+        When Clicar nos 3 botões
+        Then Clicar nos botões Edit e Delete
